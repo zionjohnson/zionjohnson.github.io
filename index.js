7a49dacd7371fa96cdb65ff04ff116338f82cb45
zion379 <zspynet22@icloud.com>
2020-06-20 12:27:16 -0700

    final changes

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const Http = new XMLHttpRequest();
+const url = "http://localhost:5500/";
+
 function swingTassle() {
-  console.log("tassle");
+  Http.open("GET", url);
+  Http.send();
 }
+
+Http.onreadystatechange = (e) => {
+  console.log(Http.responseText);
+};
